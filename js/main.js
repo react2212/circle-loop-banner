04cfe31730441e4cb0ad42fcb7d5ff764e60136d
tubi55 <tubi55@nate.com>
2023-01-23 19:03:49 +0900

    sliding panel추가

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 const banner_ul = document.querySelector('.banner ul');
 const banners = banner_ul.children;
 const btns = document.querySelector('.btns');
+const pop = document.querySelector('.pop');
+const opens = banner_ul.querySelectorAll('a');
+const close = pop.querySelector('.close');
 const [prev, next] = btns.children;
 let num = 0;
 
 init();
 prev.addEventListener('click', movePrev);
 next.addEventListener('click', moveNext);
+opens.forEach((open) => open.addEventListener('click', (e) => openPop(e)));
+close.addEventListener('click', closePop);
 
 function init() {
 	for (let i = 0; i < 3; i++) banner_ul.prepend(banner_ul.lastElementChild);
@@ -25,4 +30,19 @@ function movePrev() {
 function moveNext() {
 	banner_ul.append(banner_ul.firstElementChild);
 	activation();
+}
+
+function openPop(e) {
+	e.preventDefault();
+	const txt = e.currentTarget.closest('li').querySelector('h2').innerText;
+	pop.querySelector('h2').innerText = txt;
+	pop.classList.add('on');
+	e.currentTarget.classList.add('off');
+	btns.classList.add('off');
+}
+
+function closePop() {
+	pop.classList.remove('on');
+	btns.classList.remove('off');
+	banner_ul.querySelector('li.on a').classList.remove('off');
 }
